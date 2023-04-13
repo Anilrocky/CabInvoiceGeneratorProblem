9069ba3540c120af69dee2a13201197c63375e18
Anilrocky <anilkumarac96@gmail.com>
2023-04-13 19:50:13 +0530

    [Anil] Add:Calculate aggregate fare for multiple rides

--- CabInvoiceGeneratorTest/UnitTest1.cs
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -17,5 +17,12 @@ namespace CabInvoiceGeneratorTest
             double totalFare = this.cabInvoiceGenerator.CalculateFare(3.0, 5.0);
             Assert.AreEqual(35.0, totalFare);
         }
+        [Test]
+        public void GivenDistanceAndTimeForMultipleRides_WhenProper_ShouldReturn_AggregateFare()
+        {
+            Ride[] ride = { new Ride(3.0, 5.0), new Ride(2.0, 5.0) };
+            double invoiceSummary = this.cabInvoiceGenerator.GetMultipleRideFare(ride);
+            Assert.AreEqual(60.0, invoiceSummary);
+        }
     }
 }
